b551c1885aa7ad1390ba25b181c30d85f95005d1
Jamie Egan <j.egan@mac.com>
2021-01-18 18:26:18 +0000

    updated navbar background color

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import BannerProducts from '../components/BannerProducts'
 
 const Products = (props) => (
