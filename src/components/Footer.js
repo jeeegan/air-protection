f4b39f6b6ddc429356c7c48a6bf47800a98b5c32
Jamie Egan <j.egan@mac.com>
2021-01-18 18:34:17 +0000

    adding static folder + config

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+// import websiteLogo from '../assets/images/website-icon-logo.png';
 
 const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="copyright">
+                {/* <li><img src={websiteLogo}/> <span>Air Protection</span></li> */}
                 <li>&copy; <a href="https://www.cwcameron.co.uk">C.W. Cameron Ltd T/A Air Protection</a></li>
                 <li>Company Registration Number SC612088</li>
                 <li>Vat Registration Number GB330024564</li>
