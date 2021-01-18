952ec2f13964cad5058a03bcfc22985d5577ce78
Jamie Egan <j.egan@mac.com>
2021-01-18 13:14:25 +0000

    updating banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ const Banner = (props) => (
                 <h1>Protecting Your Workspace</h1>
             </header>
             <div className="content">
-                <p>We provide air & hand sanitisers and eco-friendly cleaning products. Protect your workplace, your people and customers. Keep your environment sanitised, healthy & safe without harmful chemicals.</p>
+                <p>We provide air, surface & hand sanitisers and eco-friendly cleaning products.<br /> Protect your workplace, your people and customers. <br />Keep your environment sanitised, healthy & safe without harmful chemicals.</p>
                 <ul className="actions">
                     <li><a href="/products" className="button next scrolly">Find out more</a></li>
                 </ul>
