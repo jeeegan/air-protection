443d33dc4084ccab1cb1b56a50612b14ab75d3fb
Jamie Egan <j.egan@mac.com>
2021-01-18 19:56:34 +0000

    updating privacy policy

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -12,9 +12,60 @@ const Privacy = (props) => (
             <section id="one">
                 <div className="inner">
                     <header className="major">
-                        <h2>Coming Soon...</h2>
+                        <h2>C.W. Cameron Ltd  Privacy Policy</h2>
+                        <h3>Last Updated 18th January 2021</h3>
                     </header>
-                    <p>Our privacy page will be updated shortly.</p>
+                    <h3>Contact Details</h3>
+                    <p> 
+Bruce Cameron<br />
+C.W. Cameron Ltd, Kirkhill House, Glasgow G77 5LL<br />
+Phone Number: 0141 237 3090<br />
+E-mail: mail@cwcameron.co.uk</p>
+<h3>The type of personal information we collect </h3>
+<p>We currently collect and process the following information:<br />
+•	Personal identifiers, contacts and characteristics (for example, name and contact details)</p>
+<h3>How we get the personal information and why we have it</h3>
+<p>Most of the personal information we process is provided to us directly by you for one of the following reasons:<br />
+•	To process orders and provide updates regarding product information, pricing and availability.<br />
+•	To allow our suppliers and delivery partners to process orders.<br />
+<br />
+We may share this information with Ecoanalytes Ltd, United Parcel Services of America Inc.<br />
+<br />
+Under the General Data Protection Regulation (GDPR), the lawful bases we rely on for processing this information are:<br />
+(a) Your consent. You are able to remove your consent at any time. You can do this by contacting mail@cwcameron.co.uk<br />
+(b) We have a contractual obligation.<br />
+(c) We have a legal obligation.<br />
+(d) We have a vital interest.<br />
+(e) We need it to perform a public task.<br />
+(f) We have a legitimate interest.</p>
+<h3>How we store your personal information </h3>
+<p>Your information is securely stored on our email servers and accounting software.
+We keep invoices and correspondence for 10 years from the last point of contact. We will then dispose your information by removing it from our systems.</p>
+<h3>Your data protection rights</h3>
+<p>Under data protection law, you have rights including:<br />
+Your right of access - You have the right to ask us for copies of your personal information.<br />
+Your right to rectification - You have the right to ask us to rectify personal information you think is inaccurate. You also have the right to ask us to complete information you think is incomplete.<br /> 
+Your right to erasure - You have the right to ask us to erase your personal information in certain circumstances.<br /> 
+Your right to restriction of processing - You have the right to ask us to restrict the processing of your personal information in certain circumstances.<br />
+Your right to object to processing - You have the the right to object to the processing of your personal information in certain circumstances.<br />
+Your right to data portability - You have the right to ask that we transfer the personal information you gave us to another organisation, or to you, in certain circumstances.<br />
+You are not required to pay any charge for exercising your rights. If you make a request, we have one month to respond to you.<br />
+Please contact us at mail@cwcameron.co.uk if you wish to make a request.</p>
+<h3>How to complain</h3>
+<p>If you have any concerns about our use of your personal information, you can make a complaint to us at mail@cwcameron.co.uk.<br />
+You can also complain to the ICO if you are unhappy with how we have used your data.<br />
+The ICO’s address:<br />            
+Information Commissioner’s Office<br />
+Wycliffe House<br />
+Water Lane<br />
+Wilmslow<br />
+Cheshire<br />
+SK9 5AF<br />
+<br />
+Helpline number: 0303 123 1113<br />
+ICO website: https://www.ico.org.uk</p>
+
+
                 </div>
             </section>
             
