4ac94eed66070e48b61afaaae871282a4e836d9f
Jamie Egan <j.egan@mac.com>
2021-01-18 12:58:35 +0000

    updated banner text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,10 +4,10 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Eco-friendly & Safe Cleaning</h1>
+                <h1>Protecting Your Workspace</h1>
             </header>
             <div className="content">
-                <p>We supply disinfecting air nebulisers and eco-friendly cleaning products. Keep your environment clean, healthy & safe without harmful chemicals.</p>
+                <p>We supply disinfecting air foggers, hand sanitiser and eco-friendly cleaning products. Keep your environment sanitised, healthy & safe without harmful chemicals.</p>
                 <ul className="actions">
                     <li><a href="/products" className="button next scrolly">Find out more</a></li>
                 </ul>
