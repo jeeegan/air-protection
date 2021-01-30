4c9d56dff029edb3976d0af4de568f4a70cd0b5d
Jamie Egan <j.egan@mac.com>
2021-01-30 13:16:28 +0000

    updated pricing page and hand sanitiser page

--- src/pages/hand_sanitiser.js
+++ src/pages/hand_sanitiser.js
@@ -3,71 +3,118 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import { Link } from 'gatsby'
 import handSanitiser from '../assets/images/handSanitiser.png'
+import dewHandSanitiserAlpha from '../assets/images/dewHandSanitiserAlpha.png'
+import dewAirAlpha from '../assets/images/dewAirAlpha.png'
+import dewDisinfectAlpha from '../assets/images/dewDisinfectAlpha.png'
+import dewReadymixAlpha from '../assets/images/dewReadymixAlpha.png'
 
 const HandSanitiser = props => (
   <Layout>
     <Helmet>
-      <title>Air Protection | Hand, Air & Surface Sanitiser Products Page</title>
-      <meta name="description" content="Hand, Air & Surface Sanitiser Products Page" />
+      <title>
+        Air Protection | Hand, Air & Surface Sanitiser Products Page
+      </title>
+      <meta
+        name="description"
+        content="Hand, Air & Surface Sanitiser Products Page"
+      />
     </Helmet>
 
     <div id="handSanitiserPage">
       <section>
         <div className="inner">
           <header className="major">
-            <img src={handSanitiser} alt="Hand, Air & Surface Sanitiser products" />
+            <img
+              src={handSanitiser}
+              alt="Hand, Air & Surface Sanitiser products"
+            />
             <h2>Hand, Air & Surface Sanitiser</h2>
             {/* <Link to="/#order" className="button next scrolly">
               Order Now
             </Link> */}
           </header>
           <p>
-            Welcome to a new range of hand sanitiser products powered by
-            electrolysed water, safer for you, kind to your skin and kinder to
-            the environment. Available in ready to use bottles as well as refillable
-            dispensers.
-          </p>
-          <p>
-            Dew Disinfect, is an eco-friendly, naturally kinder, super effective
-            and safe disinfectant that kills Germs 30 Times Faster than normal
-            household Chemicals and bleach. Utilising electrolysed water, Dew
-            Disinfect is hypoallergenic and 99.995% effective at killing
-            bacteria, such as the Flu, Listeria, Legionella, E.coli,
-            Staphylococcus, Norovirus, and much, much more. Safe to use around
-            family and pets, and no need for rubber gloves!
-          </p>
-          <p>
-            DEW disinfect hand sanitiser is bottled, ‘electrolysed’ water,
-            manufactured from deionised water and high purity salt (Sodium
-            Chloride). It offers the highest grade in multi-spectrum
+            DEW Disinfect is a new range of fast acting, non-toxic, and highly
+            effective hand, air and surface sanitisation products. It is
+            available in refillable bottles, sprays, and dispensers. Made with
+            electrolysed water and a small concentration (&#60;0.026%) of the
+            active active ingredient, Hypochlorous Acid (HOCl) - part of the
+            human/animal natural immune response, making it safe for a wide
+            range of uses. It offers the highest grade in multi-spectrum
             disinfection for your hands and any surface on which it falls.
             Additionally it deodorizes and suppresses other allergens it comes
             into contact with, but importantly, does NOT promote resistance in
             bacteria and viruses.
           </p>
           <p>
-            DEW disinfect and hand sanitiser works indiscriminately killing all
-            germs that it comes into contact with. It is effective at
-            controlling bacteria, viruses and spores.
+            Promoting the highest standard in environmental care, it is a
+            no-rinse, multi-surface general disinfectant and deodorizer that is
+            pH-neutral, and contains no toxic chemicals.
           </p>
+          <ul>
+            <li>Kills 99.995% of bacteria, viruses, fungi and spores - fast</li>
+            <li>Non-toxic disinfectant & deodorizer</li>
+            <li>Free of alcohol, aldehydes, dyes & scents, non-staining</li>
+            <li>Hypoallergenic & non-irritant</li>
+            <li>Provides rapid relief for allergy and hay-fever suffers</li>
+            <li>Leaves no residue</li>
+            <li>pH neutral / skin friendly</li>
+            <li>Cost & time effective with immediate disinfection</li>
+          </ul>
+          <div className="break"></div>
+          <img
+            src={dewHandSanitiserAlpha}
+            alt="65ml hand sanitiser"
+            style={{ width: '50%' }}
+          />
+          <div className="break"></div>
+          <h4>DEW Hand Sanitiser</h4>
+          <div className="break"></div>
           <p>
-            DEW disinfect and hand sanitiser is pH skin compatible, FREE FROM
-            harsh or toxic chemicals, Hypoallergenic and leaves no residue. It
-            remains active until it evaporates or naturally decays back to its
-            original components of just water and salt.
+            Available in 65ml pocket sized, 500ml spray bottles, and 1L wall
+            dispensers.
           </p>
+          <div className="break"></div>
+          <img
+            src={dewAirAlpha}
+            alt="185ml dew air spray"
+            style={{ width: '50%' }}
+          />
+          <div className="break"></div>
+          <h4>DEW Air</h4>
+          <div className="break"></div>
           <p>
-            DEW Air - Supplied in a specially designed trigger spray nebuliser
-            that generates a super-fine spray without the use of an aerosol. For
-            immediate use, providing rapid relief for allergy and hay-fever
-            suffers.
+            Supplied in a specially designed 185ml trigger spray nebuliser that
+            generates a super-fine spray without the use of an aerosol.
+          </p>
+          <div className="break"></div>
+          <img
+            src={dewDisinfectAlpha}
+            alt="500ml dew disinfect spray bottle"
+            style={{ width: '50%' }}
+          />
+          <div className="break"></div>
+          <h4>DEW Disinfect Surface Spray</h4>
+          <div className="break"></div>
+          <p>Supplied in a refillable 500ml spray bottle.</p>
+          <div className="break"></div>
+          <img
+            src={dewReadymixAlpha}
+            alt="5l dew readymix refil"
+            style={{ width: '50%' }}
+          />
+          <div className="break"></div>
+          <h4>DEW Disinfect Readymix</h4>
+          <div className="break"></div>
+          <p>
+            Available in 5L, 10L, 20L sizes to refill Dew Hand sanitiser, DEW
+            Air, and DEW Disinfect Surface Spray.
           </p>
           <div className="break"></div>
           <Link to="/pricing" className="button next scrolly">
             Pricing
           </Link>
         </div>
-
       </section>
     </div>
   </Layout>
--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -10,7 +10,103 @@ const Pricing = props => (
     </Helmet>
     <div id="main">
       <section id="one">
-        <h1>Coming soon...</h1>
+        <div className="inner">
+          <header className="major">
+            <h2>Price List</h2>
+          </header>
+          <table>
+              <tr>
+                  <th>Description</th>
+                  <th>Unit Price (ex VAT)*</th>
+              </tr>
+              <tr>
+                  <th>Foggers & Misters</th>
+              </tr>
+              <tr>
+                  <td>Office / Home Dry Mister (with 2 × 5L Disinfect Refill)</td>
+                  <td>£95.83</td>
+              </tr>
+              <tr>
+                  <td>Fogging Machine - Ultra Low Volume Nebuliser</td>
+                  <td>£495.00</td>
+              </tr>
+              <tr>
+                  <th>Hand, Air & Surface Sanitiser</th>
+              </tr>
+              <tr>
+                  <td>Dew Air 185ml - Fine Mist Room Spray</td>
+                  <td>£5.83</td>
+              </tr>
+              <tr>
+                  <td>Dew Disinfect 500ml - Surface Spray</td>
+                  <td>£4.16</td>
+              </tr>
+              <tr>
+                  <td>Dew Hand Sanitiser 65ml - Pocket Sized</td>
+                  <td>£2.91</td>
+              </tr>
+              <tr>
+                  <td>Dew Hand Sanitiser 500ml</td>
+                  <td>£4.16</td>
+              </tr>
+              <tr>
+                  <td>Dew Disinfect Refill 5L (Disinfect, Air & Hand Sanitiser)</td>
+                  <td>£11.67</td>
+              </tr>
+              <tr>
+                  <td>Dew Disinfect Refill 10L with Tap (Disinfect, Air & Hand Sanitiser)</td>
+                  <td>£22.50</td>
+              </tr>
+              <tr>
+                  <td>Dew Disinfect Refill 20L with Tap (Disinfect, Air & Hand Sanitiser)</td>
+                  <td>£38.50</td>
+              </tr>
+              <tr>
+                  <td>Dew Hand Sanitiser Dispenser – Wall Mounted</td>
+                  <td>£12.00</td>
+              </tr>
+              <tr>
+                  <td>Dew Hand Sanitiser Dispenser – Refill 1L</td>
+                  <td>£6.75</td>
+              </tr>
+              <tr>
+                  <th>Clean Protection</th>
+              </tr>
+              <tr>
+                  <td>Dew Superclean 500ml - Powerful Multi-Purpose Cleaner/Degreaser Spray</td>
+                  <td>£4.16</td>
+              </tr>
+              <tr>
+                  <td>Dew Foam 200ml - Hand & Body Wash</td>
+                  <td>£3.74</td>
+              </tr>
+              <tr>
+                  <td>Dew Foam 250ml - Hand & Body Wash (25% Extra Special Offer)</td>
+                  <td>£3.74</td>
+              </tr>
+              <tr>
+                  <td>Dew Superclean Refill 5L (Superclean & Foam)</td>
+                  <td>£11.67</td>
+              </tr>
+              <tr>
+                  <td>Dew Superclean Refill 10L with Tap (Superclean & Foam)</td>
+                  <td>£22.50</td>
+              </tr>
+              <tr>
+                  <td>Dew Superclean Refill 10L with Cap (Superclean & Foam)</td>
+                  <td>£20.00</td>
+              </tr>
+              <tr>
+                  <td>Dew Superclean Refill 20L with Tap (Superclean & Foam)</td>
+                  <td>£38.50</td>
+              </tr>
+              <tr>
+                  <td>Dew Superclean Refill 20L with Cap (Superclean & Foam)</td>
+                  <td>£34.17</td>
+              </tr>
+          </table>
+          <p>* Contract/bulk pricing available on request</p>
+        </div>
       </section>
     </div>
   </Layout>
